58c8db5f58e3b3fdaf5196dea3dc4ed550ef681f
Juan Ayala <juan.ayala@akqa.com>
2015-02-19 18:02:29 -0500

    got rid of windows update

--- scripts/enable-winrm.bat
+++ scripts/enable-winrm.bat
@@ -1,15 +1,18 @@
-winrm quickconfig -q
-winrm quickconfig -transport:http
-winrm set winrm/config @{MaxTimeoutms="1800000"}
-winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-winrm set winrm/config/service/auth @{Basic="true"}
-winrm set winrm/config/client/auth @{Basic="true"}
-winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
+rem enable winrm
+cmd.exe /c winrm quickconfig -q
+cmd.exe /c winrm quickconfig -transport:http
+cmd.exe /c winrm set winrm/config @{MaxTimeoutms="1800000"}
+cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
+cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
+cmd.exe /c winrm set winrm/config/client/auth @{Basic="true"}
+cmd.exe /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
 
-netsh advfirewall firewall set rule group="remote administration" new enable=yes
-netsh firewall add portopening TCP 5985 "Port 5985"
+rem add firewall rule
+cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
+cmd.exe /c netsh firewall add portopening TCP 5985 "Port 5985"
 
-net stop winrm
-sc.exe config winrm start= auto
-net start winrm
+rem restart service
+cmd.exe /c net stop winrm
+cmd.exe /c sc config winrm start= auto
+cmd.exe /c net start winrm
