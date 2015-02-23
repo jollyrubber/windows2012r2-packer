e85a75d0cde76827957dc45a5b4999a11e148e53
Juan Ayala <juan.ayala@akqa.com>
2015-02-23 12:18:05 -0500

    added compact.ps1 converted vagrant-ssh.bat to vagrant-ssh.ps1 remove auto logon in Autounattend.xml at end of synchronous commands

---
+++ scripts/compact.ps1
@@ -0,0 +1,48 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+
+# Ultra Defrag
+$pathToZip = "C:\Windows\Temp\ultradefrag.zip"
+if(!(Test-Path -Path $pathToZip)) {
+    # download
+    Write-Verbose "downloading ultradefrag"
+    (New-Object System.Net.WebClient). `
+        DownloadFile("http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-6.0.2.bin.amd64.zip", `
+                     $pathToZip)
+}
+$ultradefrag = "C:\Windows\Temp\ultradefrag-portable-6.0.2.amd64\udefrag.exe"
+if(!(Test-Path -Path $ultradefrag)) {
+    # extract
+    Write-Verbose "extracting ultradefrag"
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, "C:\Windows\Temp")
+}
+# run
+Start-Process "$ultradefrag" -ArgumentList "--optimize --repeat C:" -NoNewWindow -Wait
+
+# SDelete
+$pathToZip = "C:\Windows\Temp\SDelete.zip"
+if(!(Test-Path -Path $pathToZip)) {
+    # download
+    Write-Verbose "downloading sdelete"
+    (New-Object System.Net.WebClient). `
+        DownloadFile("http://download.sysinternals.com/files/SDelete.zip", `
+                     $pathToZip)
+}
+$sdelete = "C:\Windows\Temp\sdelete.exe"
+if(!(Test-Path -Path $sdelete)) {
+    # extract
+    Write-Verbose "extracting sdelete"
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, "C:\Windows\Temp")
+}
+# run
+$sdeleteRegKey = "HKCU:\Software\Sysinternals\SDelete"
+if(!(Test-Path -Path $sdeleteRegKey)) {
+    New-Item -Path $sdeleteRegKey
+    New-ItemProperty -Path $sdeleteRegKey -Name "EulaAccepted" -Value 1 -PropertyType "DWord"
+}
+Start-Process "$sdelete" -ArgumentList "-q -z C:" -NoNewWindow -Wait
+Remove-Item $sdeleteRegKey
+
+# clean out windows updates
+Stop-Service wuauserv
+Get-ChildItem -Path "C:\Windows\SoftwareDistribution\Download" -Include * | Remove-Item -Recurse
+Start-Service wuauserv
---
+++ scripts/vagrant-ssh.ps1
@@ -0,0 +1,11 @@
+﻿$userPath = "C:\Users\vagrant\.ssh"
+if(Test-Path -Path "a:\vagrant.pub") {
+    Write-Verbose "getting vagrant.pub from floppy"
+    Copy-Item -Path "a:\vagrant.pub" -Destination "$userPath\authorized_keys"
+}
+else {
+    Write-Verbose "downloading vagrant.pub"
+    (New-Object System.Net.WebClient). `
+        DownloadFile("https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub", `
+                     "$userPath\authorized_keys")
+}
