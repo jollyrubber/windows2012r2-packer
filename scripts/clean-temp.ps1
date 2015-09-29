fe596639d80a4e7b4edd174949e90a93fbbf3dae
juan.ayala.tech@gmail.com <juan.ayala@akqa.com>
2015-09-29 12:29:11 -0400

    added clean-temp.ps1 and updated vm-guest-tools to download iso instead of working from attached media

---
+++ scripts/clean-temp.ps1
@@ -0,0 +1,3 @@
+$tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*")
+
+Remove-Item $tempfolders -Force -Recurse -ErrorAction SilentlyContinue
--- scripts/vm-guest-tools.ps1
+++ scripts/vm-guest-tools.ps1
@@ -1,13 +1,37 @@
+Import-Module BitsTransfer
+
 if([string]::Compare($env:PACKER_BUILDER_TYPE, "virtualbox-iso", $True) -eq 0) {
 
-    $driveLetter = "E"
+    $vboxVersion = Get-Content ~\.vbox_version
+    $isoFile = [string]::Format("VBoxGuestAdditions_{0}.iso", $vboxVersion)
+    $isoUrl = [string]::Format("http://download.virtualbox.org/virtualbox/{0}/{1}", $vboxVersion, $isoFile)
 
+    Push-Location
+    Set-Location $env:TEMP
+
+    # download ISO
+    if(!(Test-Path -Path $isoFile)) {
+            Start-BitsTransfer -Source $isoUrl -Destination $isoFile
+    }
+
+    # mount
+    $imagePath = Join-Path $env:TEMP $isoFile
+    Mount-DiskImage -ImagePath $imagePath
+    # copy
+    $source = (Get-DiskImage $imagePath | Get-Volume).DriveLetter + ":\"
+    $target = Join-Path $env:TEMP "virtualbox"
+    Remove-Item $target -Recurse -Force -ErrorAction SilentlyContinue
+    Copy-Item $source $target -Recurse
+    # unmount
+    Dismount-DiskImage $imagePath
+    Remove-Item -Path $imagePath -Force
     # install cert
-    $certUtil = $driveLetter + ":\cert\VBoxCertUtil.exe"
-    $cert = $driveLetter + ":\cert\oracle-vbox.cer"
+    $certUtil = Join-Path $target "cert\VBoxCertUtil.exe"
+    $cert = Join-Path $target "cert\oracle-vbox.cer"
     &$certUtil -v add-trusted-publisher $cert
-
     # install addtions
-    $additions = $driveLetter + ":\VBoxWindowsAdditions.exe"
+    $additions = Join-Path $target "VBoxWindowsAdditions.exe"
     &$additions  /S
+
+    Pop-Location
 }
